e4da68123ecd71738c03f7c28d829208a8b36a95
kamillel <kamille.lem@gmail.com>
2015-01-06 22:08:15 +0000

    use ngGate8Masonry directive to fix content overlapping

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
-var december = angular.module('december', ['wu.masonry']);
-// var december = angular.module('december', []);
+var december = angular.module('december', []);
+// var december = angular.module('december', ['ng']);
 
 december.controller('VocabController', function($scope, $http) {
     var vocab = this;
@@ -11,4 +11,28 @@ december.controller('VocabController', function($scope, $http) {
     .error(function (data) {
         // 残念! json loading fail!
     });
+});
+
+// directive from https://github.com/gate8team/socialMood
+december.directive('ngGate8Masonry', function($timeout) {
+    return function(scope, element, attrs) {
+
+        if (scope.$last){
+            $timeout(function () {
+                var parent = element.parent();
+                var masonry = new Masonry(parent[0], {
+                    itemSelector: '.word',
+				    gutter: 10,
+                    isAnimated: true,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false
+                    },
+                    transitionDuration : "0.4s",
+                    isResizable: false
+                });
+            });
+        }
+    };
 });
